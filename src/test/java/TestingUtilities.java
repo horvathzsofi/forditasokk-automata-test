85f0b58b9d7988207b22aa567d311e7b2a4ce869
horvathzsofi <71212886+horvathzsofi@users.noreply.github.com>
2022-06-12 01:46:20 +0200

    Setup web driver with BeforeEach & AfterEach methods

---
+++ src/test/java/TestingUtilities.java
@@ -0,0 +1,34 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestingUtilities {
+    WebDriver webDriver = null;
+
+    @BeforeEach
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("start-maximized");
+
+        webDriver = new ChromeDriver(options);
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        webDriver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void quitDriver(){
+        webDriver.quit();
+    }
+}
