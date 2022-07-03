2e9f90851179e3a1549424001aa39e2c1e6ef771
horvathzsofi <71212886+horvathzsofi@users.noreply.github.com>
2022-07-03 21:03:58 +0200

    updated TestRegistration

--- src/test/java/TestRegistration.java
+++ src/test/java/TestRegistration.java
@@ -29,26 +29,19 @@ public class TestRegistration extends TestingSetup {
     @Description("Sikeres regisztráció, mivel mindegyik beviteli mező valid adatot tartalmaz (DT-1 alapján)")
     @Severity(SeverityLevel.CRITICAL)
     public void successfulRegistration() throws InterruptedException {
-        Logger logger = LoggerFactory.getLogger(TestRegistration.class);
-        logger.info("[successfulRegistration] method started.");
         String username = "bakipo3967";
         String email = "bakipo3967@jrvps.com";
         String password = "a";
         boolean accept = true;
         registration.navigateTo();
-        logger.info("[successfulRegistration] navigated to registration page.");
         registration.enterUsername(username);
-        logger.info("[successfulRegistration] entered username: " + username + ".");
         registration.enterEmail(email);
-        logger.info("[successfulRegistration] entered email: " + email + ".");
         registration.enterPassword(password);
         registration.acceptPrivacyPolicy(accept);
-        logger.info("[successfulRegistration] accepted Privacy policy: " + accept + ".");
         registration.clickOnRegistrationButton();
-        logger.info("[successfulRegistration] clicked on registration button.");
 
         Thread.sleep(2000);
-        Allure.addAttachment("Képernyőkép a sikeres regisztrációról", new ByteArrayInputStream(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES)));
+
         boolean messageDisplayed = registration.isMessageDisplayed();
         String expectedMessage = "Az aktiváláshoz szükséges azonosító linket elküldtük a megadott e-mail címre.\n" +
                 "Az aktiváláshoz kattints rá, vagy másold a böngésző címsorába.";
@@ -56,7 +49,7 @@ public class TestRegistration extends TestingSetup {
 
         Assertions.assertTrue(messageDisplayed);
         Assertions.assertEquals(expectedMessage, actualMessage);
-        logger.info("[successfulRegistration] method finished.");
+
         Allure.addAttachment("Képernyőkép a sikeres regisztrációról", new ByteArrayInputStream(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES)));
     }
 
