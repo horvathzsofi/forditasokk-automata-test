6bf10aa15b4ed8a3d93aa226b6aea7e23071957f
horvathzsofi <71212886+horvathzsofi@users.noreply.github.com>
2022-07-04 00:07:58 +0200

    Write testcases for Logout

--- src/main/java/Logout.java
+++ src/main/java/Logout.java
@@ -1,7 +1,32 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Logout extends PageBase{
-    protected Logout(WebDriver driver) {
+    private final String url = "http://forditasokk.probaljaki.hu/felhasznalo/forditasaim.php";
+    private final By logoutLink = By.className("logout");
+    private final By userMenu = By.xpath("//*[@class=\"felhasznalo\"]//a");
+
+    public Logout(WebDriver driver) {
         super(driver);
+    }
+
+    public void navigateTo(){
+        webDriver.navigate().to(url);
+    }
+
+    public void clickOnLogoutLink(){
+        webDriver.findElement(logoutLink).click();
+    }
+
+    public String[] getUserMenuItems(){
+        List<WebElement> userMenuItems = webDriver.findElements(userMenu);
+        String[] userMenuLinks = new String[userMenuItems.size()];
+        for (int i = 0; i < userMenuItems.size(); i++) {
+            userMenuLinks[i] = userMenuItems.get(i).getText();
+        }
+        return userMenuLinks;
     }
 }
--- src/test/java/TestLogout.java
+++ src/test/java/TestLogout.java
@@ -1,7 +1,18 @@
+import io.qameta.allure.Allure;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import java.io.ByteArrayInputStream;
+
+@Feature("Kijelentkezés")
 public class TestLogout extends TestingSetup{
     Logout logout = null;
 
@@ -11,8 +22,21 @@ public class TestLogout extends TestingSetup{
     }
 
     @Test
-    public void successfulLogout(){
+    @DisplayName("Kijelentkezés tesztelése")
+    @Description("Kijelentkezés felhasználói fiókból")
+    @Severity(SeverityLevel.CRITICAL)
+    public void successfulLogout() throws InterruptedException {
+        TestLogin login = new TestLogin();
+        login.successfulLogin();
+        logout.navigateTo();
+        String[] beforeLogoutUserMenu = logout.getUserMenuItems();
+        Allure.addAttachment("Képernyőkép a kijelentkezés előtti állapotról", new ByteArrayInputStream(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES)));
+        logout.clickOnLogoutLink();
+        String[] afterLogoutUserMenu = logout.getUserMenuItems();
 
+        Assertions.assertNotEquals(beforeLogoutUserMenu.toString(), afterLogoutUserMenu.toString());
+
+        Allure.addAttachment("Képernyőkép a kijelentkezés előtti állapotról", new ByteArrayInputStream(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES)));
     }
 
 }
