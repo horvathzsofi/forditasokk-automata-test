5f70e0733ab9ccfa888d4a98487107a5bab6739c
horvathzsofi <71212886+horvathzsofi@users.noreply.github.com>
2022-07-05 01:30:16 +0200

    Updated Entertainments and TestEntertainments

--- src/main/java/Entertainments.java
+++ src/main/java/Entertainments.java
@@ -1,7 +1,29 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Entertainments extends PageBase{
-    protected Entertainments(WebDriver driver) {
+    private final String url = "http://forditasokk.probaljaki.hu/kiadok/kiadok.php";
+    private final By entertainmentNames = By.className("kartya_cim");
+
+    public Entertainments(WebDriver driver) {
         super(driver);
+    }
+
+    public void navigateToPage(){
+        webDriver.navigate().to(url);
+    }
+
+    public String[] getEntertainmentsName(){
+        List<WebElement> entertainmentsElements = webDriver.findElements(entertainmentNames);
+        String[] entertainmentNames = new String[entertainmentsElements.size()];
+
+        for (int i = 0; i < entertainmentsElements.size(); i++) {
+            entertainmentNames[i] = entertainmentsElements.get(i).getText();
+        }
+
+        return entertainmentNames;
     }
 }
--- src/test/java/TestEntertainments.java
+++ src/test/java/TestEntertainments.java
@@ -1,7 +1,14 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
+@Feature("Adatok kilistázása")
 public class TestEntertainments extends TestingSetup {
     Entertainments entertainments = null;
 
@@ -10,18 +17,42 @@ public class TestEntertainments extends TestingSetup {
         entertainments = new Entertainments(webDriver);
     }
 
-    /** TODO:
+    /**
+     * TODO:
      * Test steps:
      * 1. Navigate to the website
      * 2. Count entertainments
      * 3. Verify that all the predefined entertainments are displayed on the page
-     *
-     * Predefined entertainments: Big Hit, Choeun, Cube, Dream Tea, Fantagio, FNC, Hyeyoom, JTM, JYP, N\A, Pledis, RBW Entertainment, SM, YG
      */
 
-    @Disabled
-    @Test
-    public void verifyThatAllEntertainmentsAreDisplayed(){
 
+    @Test
+    @DisplayName("Kiadók nevének kilistázása")
+    @Description("Az összes kiadó nevének kilistázása")
+    @Severity(SeverityLevel.MINOR)
+    public void verifyThatAllEntertainmentsAreDisplayed() {
+        int expectedLength = 14;
+        String[] expectedEntertainmentNames = {
+                "Big Hit",
+                "Choeun",
+                "Cube",
+                "Dream Tea",
+                "Fantagio",
+                "FNC",
+                "Hyeyoom",
+                "JTM",
+                "JYP",
+                "N\\A",
+                "Pledis",
+                "RBW Entertainment",
+                "SM",
+                "YG"
+        };
+
+        entertainments.navigateToPage();
+        String[] actualEntertainmentNames = entertainments.getEntertainmentsName();
+
+        Assertions.assertEquals(expectedLength, actualEntertainmentNames.length);
+        Assertions.assertArrayEquals(expectedEntertainmentNames, actualEntertainmentNames);
     }
 }
