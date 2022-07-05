f0110386603e15886ebe0ed292f81f48dd2456f9
horvathzsofi <71212886+horvathzsofi@users.noreply.github.com>
2022-07-06 01:01:23 +0200

    Updated Musics and TestMusics files

--- src/main/java/Musics.java
+++ src/main/java/Musics.java
@@ -7,27 +7,78 @@ import java.util.List;
 public class Musics extends PageBase {
     private final String url = "http://forditasokk.probaljaki.hu/zenek/zenek.php";
     private final By musicCard = By.className("kartya");
-    private final By cardTitle = By.className("kartya_cim");
     private final By addMusicLink = By.linkText("Zene hozzáadása");
     private final By artistName = By.name("eloado");
     private final By albumTitle = By.name("album");
     private final By songTitle = By.name("dal_cim");
-    private final By titleSong = By.name("focimdal");
+    private final By itIsATitleSong = By.id("focimdal_igen");
+    private final By notATitleSong = By.id("focimdal_nem");
     private final By addMusicButton = By.name("zene_hozzaad");
 
-    /*
-    //*[@class="kartya"]//*[text()="asd"] - verify if the song was added
-    //*[@class="kartya_cim"][contains(text(),"Cold Love")] - find a card with this title
-     */
+    private final By editMusicLink = By.linkText("Zene szerkesztése");
+    private final By editSongTitle = By.name("zene_cime");
+    private final By updateMusicButton = By.name("zene_frissit");
 
 
     public Musics(WebDriver driver) {
         super(driver);
     }
 
-    public List<WebElement> getMusicCards(){
+    public void navigateToPage() {
+        webDriver.navigate().to(url);
+    }
+
+    public List<WebElement> getMusicCards() {
         return webDriver.findElements(musicCard);
     }
 
+    public WebElement findElementWithTitle(String musicTitle) {
+        String dynamicXpath = "//*[text()=\"" + musicTitle + "\"]/parent::a";
+        return webDriver.findElement(By.xpath(dynamicXpath));
+    }
 
+    public void clickOnMusicCardWithTitle(String musicTitle) {
+        findElementWithTitle(musicTitle).click();
+    }
+
+    public void clickOnAddMusicLink() {
+        webDriver.findElement(addMusicLink).click();
+    }
+
+    public void enterArtistName(String testData) {
+        webDriver.findElement(artistName).sendKeys(testData);
+    }
+
+    public void enterAlbumTitle(String testData) {
+        webDriver.findElement(albumTitle).sendKeys(testData);
+    }
+
+    public void enterSongTitle(String testData) {
+        webDriver.findElement(songTitle).sendKeys(testData);
+    }
+
+    public void isThisATitleSong(boolean testData) {
+        if (testData) {
+            webDriver.findElement(itIsATitleSong).click();
+        } else {
+            webDriver.findElement(notATitleSong).click();
+        }
+    }
+
+    public void clickOnAddMusicButton() {
+        webDriver.findElement(addMusicButton).click();
+    }
+
+    public void clickOnEditMusicLink() {
+        webDriver.findElement(editMusicLink).click();
+    }
+
+    public void enterSongTitleToUpdate(String testData) {
+        webDriver.findElement(editSongTitle).clear();
+        webDriver.findElement(editSongTitle).sendKeys(testData);
+    }
+
+    public void clickOnUpdateMusicButton() {
+        webDriver.findElement(updateMusicButton).click();
+    }
 }
--- src/test/java/TestMusics.java
+++ src/test/java/TestMusics.java
@@ -1,25 +1,79 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@Feature("Műveletek a Zene oldalon")
 public class TestMusics extends TestingSetup {
     Musics musics = null;
+    Login login = null;
 
     @BeforeEach
     public void createMusic() {
         musics = new Musics(webDriver);
+        login = new Login(webDriver);
     }
 
-    @Disabled
     @Test
-    public void addNewMusic(){
+    @DisplayName("Új zene hozzáadása az adatbázishoz")
+    @Description("Új zene hozzáadása az adatbázishoz")
+    @Severity(SeverityLevel.NORMAL)
+    public void addNewMusic() {
+        String username = "felhasznalo";
+        String password = "asd";
+        String artistName = "ONEWE";
+        String albumTitle = "Planet Nine : Alter Ego";
+        String songTitle = "Rain To Be";
+        boolean isTitleSong = true;
 
+        login.navigateToPage();
+        login.enterUsername(username);
+        login.enterPassword(password);
+        login.clickOnButton();
+
+        musics.navigateToPage();
+        int numberOfMusicsBeforeAddingNewOne = musics.getMusicCards().size();
+        musics.clickOnAddMusicLink();
+        musics.enterArtistName(artistName);
+        musics.enterAlbumTitle(albumTitle);
+        musics.enterSongTitle(songTitle);
+        musics.isThisATitleSong(isTitleSong);
+        musics.clickOnAddMusicButton();
+        int numberOfMusicsAfterAddingNewOne = musics.getMusicCards().size();
+
+        Assertions.assertNotEquals(numberOfMusicsAfterAddingNewOne, numberOfMusicsBeforeAddingNewOne);
+        Assertions.assertTrue(musics.findElementWithTitle(songTitle).isDisplayed());
     }
 
 
-    @Disabled
     @Test
-    public void modifyMusicDetails(){
-        
+    @DisplayName("Meglévő adat módosítása")
+    @Description("Meglévő adat módosítása")
+    @Severity(SeverityLevel.NORMAL)
+    public void modifyMusicDetails() {
+        String username = "felhasznalo";
+        String password = "asd";
+        String oldSongTitle = "Don't Wanna Cry";
+        String newSongTitle = "Baby don't cry";
+
+        login.navigateToPage();
+        login.enterUsername(username);
+        login.enterPassword(password);
+        login.clickOnButton();
+
+        musics.navigateToPage();
+        int numberOfMusicsBeforeUpdating = musics.getMusicCards().size();
+        musics.clickOnMusicCardWithTitle(oldSongTitle);
+        musics.clickOnEditMusicLink();
+        musics.enterSongTitleToUpdate(newSongTitle);
+        musics.clickOnUpdateMusicButton();
+        int numberOfMusicsAfterUpdating = musics.getMusicCards().size();
+
+        Assertions.assertEquals(numberOfMusicsBeforeUpdating,numberOfMusicsAfterUpdating);
+        Assertions.assertTrue(musics.findElementWithTitle(newSongTitle).isDisplayed());
     }
 }
