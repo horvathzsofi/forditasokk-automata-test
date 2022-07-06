9fdff5b2a3553934cbaa2a5a08b5a5bf35be1fa5
horvathzsofi <71212886+horvathzsofi@users.noreply.github.com>
2022-07-06 23:37:40 +0200

    updated TestArtists

--- src/test/java/TestArtists.java
+++ src/test/java/TestArtists.java
@@ -4,7 +4,6 @@ import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -33,8 +32,6 @@ public class TestArtists extends TestingSetup {
 
     }
 
-    // the content of the csv file was already added
-    @Disabled
     @ParameterizedTest
     @DisplayName("Előadók sorozatos hozzáadása az adatbázishoz külső adatforrásból")
     @Description("Előre meghatározott előadók hozzáadása csv kiterjesztésű fájlból")
